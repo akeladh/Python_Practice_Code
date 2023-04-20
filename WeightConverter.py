37ac24d70e31eef7f3f3865c64c02a344d11214f
Akela <akela.doho@gmail.com>
2023-04-19 22:26:51 -0700

    Fixed Weight Converter Exercise

--- WeightConverter.py
+++ WeightConverter.py
@@ -4,18 +4,18 @@
 
 #Input
 user_weight = input("Hello there what is your weight?: ")
-measurement = input("(L)lbs or (K)g: ")
+measurement = input("(L)bs or (K)g: ")
 
 #Convert the weight to an int
 user_weight = int(user_weight)
 
 #check to see if upper/lower case L or K
 unit = ""
-if measurement == 'l' or 'L':
+if measurement == 'l' or measurement == 'L':
     user_weight *= 0.45
     unit = "Kgs"
-elif measurement == 'k' or 'K':
-    user_weight *= 2.2
+elif measurement == 'k' or measurement == 'K':
+    user_weight *= 2.20
     unit = "Pounds"
 else:
     print("Invalid value")
