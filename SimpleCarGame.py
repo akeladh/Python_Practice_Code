5788c038c2ede3a2e28b0f4b22f429ba5ae564ba
Akela <akela.doho@gmail.com>
2023-04-19 23:11:52 -0700

    Car Game successfully runs

--- SimpleCarGame.py
+++ SimpleCarGame.py
@@ -3,19 +3,22 @@
 #User can exit out of game at any moment when quit command is typed
 #User can have menu pop up of command options when help commands is typed
 #Any other command will not be accepted
-command = input(">")
-while command != 'quit':
+command = ''
+#while command.lower() != 'quit': -- you can write this but it's repeating step 20
+while True: #while this block of code is True
+    command = input("> ")
     if command == 'help':
         print('''
         start - to start the car 
         stop - to stop the car 
         quit - to exit
         ''')
-    if command == 'start':
+    elif command == 'start':
         print('Car started...Ready to go!')
     elif command == 'stop':
         print('Car stopped!')
     elif command == 'quit':
+        print('You have exited the game')
         break
     else:
         print("I don't understand")
